11c3e4d4885ee9267b02e5d6272acb45750ce8f5
Phil Carbone <phil.carbone@virteom.com>
2017-03-05 17:22:11 -0500

    removed unnessecarry lines of code

--- CoreV2.php
+++ CoreV2.php
@@ -1,8 +1,5 @@
 <?php
-
 namespace BumperLane\Api\ClientModule;
-include_once(__DIR__ . '/../includes.php');
-
 class CoreV2 extends \BumperLane\Api\Client\Core\ClientModuleBase {
     public function __construct(){
         $this->Api = "core/v2";
