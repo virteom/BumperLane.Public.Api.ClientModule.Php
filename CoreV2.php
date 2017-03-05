574202875ecf80e801be430420fbb1a4a33063d0
Phil Carbone <phil.carbone@virteom.com>
2017-03-05 13:50:06 -0500

    added namspace to inherited class

--- CoreV2.php
+++ CoreV2.php
@@ -3,7 +3,7 @@
 namespace BumperLane\Api\ClientModule;
 include_once(__DIR__ . '/../includes.php');
 
-class CoreV2 extends ClientModuleBase {
+class CoreV2 extends \BumperLane\Api\Client\Core\ClientModuleBase {
     public function __construct(){
         $this->Api = "core/v2";
     }
